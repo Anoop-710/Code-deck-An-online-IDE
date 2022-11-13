400e39288c980c7978f44d895ce096cf133b78ec
anoop <anoopaathreya@gmail.com>
2022-11-13 12:00:43 +0530

    Added dark mode

--- src/screens/Home/LeftComponent.jsx
+++ src/screens/Home/LeftComponent.jsx
@@ -8,10 +8,13 @@ const StyledLeftComponent = styled.div`
     left: 0;
     width: 40%;
     height: 100vh;
-    background-color: #241f21;
     display: flex;
     justify-content: center;
     align-items: center;
+    background: #0F2027;  /* fallback for old browsers */
+    background: -webkit-linear-gradient(to right, #2C5364, #203A43, #0F2027);  /* Chrome 10-25, Safari 5.1-6 */
+    background: linear-gradient(to right, #2C5364, #203A43, #0F2027); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+
 `
 
 const ContentContainer = styled.div`
--- src/screens/Home/RightComponent.jsx
+++ src/screens/Home/RightComponent.jsx
@@ -4,13 +4,123 @@ import { HiOutlineTrash } from 'react-icons/hi';
 import { BiEditAlt } from 'react-icons/bi'
 import logo from '../../Assets/logo-small.png';
 
+
 const StyledRightComponent = styled.div`
+    
+  
+  span {
+    font-size: 20px
+  }
+  
+  .dark-mode {
+    background-color: #1a1919;
+    color: white;
+    // height: 100%;
+    transition: background-color 0.25s ease-in-out;
     position: absolute;
     top: 0;
     right: 0;
     width: 60%;
     padding: 2rem;
+    // height: 100vh;
+    
+  }
+  
+  .light-mode {
+    background-color: #fff;
+    color: #333;
+    // height: 100%;
+    transition: background-color 0.25s ease-in-out;
+
+    position: absolute;
+    top: 0;
+    right: 0;
+    width: 60%;
+    padding: 2rem;
+    // height: 100vh;
+
+  }
+  
+  .container {
+    position: absolute;
+    top: -5px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    
+  }
+  
+
+  
+  .switch {
+    position: relative;
+    display: inline-block;
+    width: 60px;
+    height: 34px;
+  }
+  
+  .switch input { 
+    opacity: 0;
+    width: 0;
+    height: 0;
+  }
+  
+  .slider {
+    position: absolute;
+    cursor: pointer;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    background-color: #ccc;
+    -webkit-transition: .4s;
+    transition: .4s;
+  }
+  
+  .slider:before {
+    position: absolute;
+    content: "";
+    height: 26px;
+    width: 26px;
+    left: 4px;
+    bottom: 4px;
+    background-color: white;
+    -webkit-transition: .4s;
+    transition: .4s;
+  }
+  
+  input:checked + .slider {
+    background-color: #19a2c1;
+  }
+  
+  input:focus + .slider {
+    box-shadow: 0 0 1px #2196F3;
+  }
+  
+  input:checked + .slider:before {
+    -webkit-transform: translateX(26px);
+    -ms-transform: translateX(26px);
+    transform: translateX(26px);
+  }
+  
+  /* Rounded sliders */
+  .slider.round {
+    border-radius: 34px;
+  }
+  
+  .slider.round:before {
+    border-radius: 50%;
+  }
+  .switch-checkbox {
+    padding: 10px
+  }
+    // position: absolute;
+    top: 0;
+    right: 0;
+    width: 60%;
+    padding: 2rem;
     height: 100vh;
+    border: 2px solid red;
 `
 const Header = styled.div`
   display: flex;
@@ -29,7 +139,7 @@ const Heading = styled.h3`
 const AddFolder = styled.div`
     font-size: 1rem;
     border-radius: 30px;
-    color: black;
+    color: dark-gray;
     display: flex;
     align-items: center;
     gap: 0.25rem;
@@ -81,64 +191,86 @@ const Logo = styled.img`
 `
 
 function RightComponent() {
-  return (
-    <StyledRightComponent>
-        <Header>
-            <Heading size="large">
-            My Playground
-            </Heading>
-            <AddFolder><span>+</span> New Folder</AddFolder>
-        </Header>
-        <hr />
+    const [darkMode,setDarkMode] = React.useState(false);
 
-        {
-            Array.from({length:4}).map(()=>(
-                <FolderCard>
+  return (
+    <>
+        
+        <StyledRightComponent>
+        <div className={darkMode ? 'dark-mode' : 'light-mode '}>
+            <div className="container">
+                <span style={{color: darkMode? 'grey' : 'white'}}>☀️</span>
+                <div className='switch-checkbox'>
+                    <label className='switch'>
+                    <input type="checkbox"  onChange={() => setDarkMode(!darkMode) } />
+                    <span className='slider round'></span>
+                    </label>
+                </div>
+                
+                <span style={{color: darkMode? 'white' : 'grey'}}>🌙</span>
+            </div>
+        <div>
             <Header>
-                <Heading size="small">
-                    Folder Name
+                <Heading size="large">
+                My Playground
                 </Heading>
-                <FolderIcons>
+                <AddFolder><span>+</span> New Folder</AddFolder>
+            </Header>
+            <hr />
+
+            {
+                Array.from({length:4}).map(()=>(
+                    <FolderCard>
+                <Header>
+                    <Heading size="small">
+                        Folder Name
+                    </Heading>
+                    <FolderIcons>
+                        <HiOutlineTrash />
+                        <BiEditAlt />
+                        <AddFolder><span>+</span> New Playground</AddFolder>
+                    </FolderIcons>
+                </Header>
+
+
+            
+                <PlayGroundCards>
+
+                    {
+                    Array.from({length:4}).map(()=>(
+                        <Card>
+                    <CardContainer>
+                        <Logo src={logo}/>
+                        <CardContent>
+                            <p>Playground name</p>
+                            <p>Language: Java</p>
+                        </CardContent>
+                    </CardContainer>
+                    
+
+                    <FolderIcons>
                     <HiOutlineTrash />
                     <BiEditAlt />
-                    <AddFolder><span>+</span> New Playground</AddFolder>
-                </FolderIcons>
-            </Header>
-
-
-           
-            <PlayGroundCards>
-
-                {
-                Array.from({length:4}).map(()=>(
-                    <Card>
-                <CardContainer>
-                    <Logo src={logo}/>
-                    <CardContent>
-                        <p>Playground name</p>
-                        <p>Language: Java</p>
-                    </CardContent>
-                </CardContainer>
-                
-
-                <FolderIcons>
-                <HiOutlineTrash />
-                <BiEditAlt />
+                        
+                    </FolderIcons>
+                    </Card>
+                    ))
                     
-                </FolderIcons>
-                </Card>
-                ))
-                
-                }
-                
+                    }
+                    
 
-                
-            </PlayGroundCards>
-        </FolderCard>
-            ))
-        }
-        
-    </StyledRightComponent>
+                    
+                </PlayGroundCards>
+            </FolderCard>
+                ))
+            }
+            </div>
+        </div>
+            
+            
+        </StyledRightComponent>
+    </>
+    
   )
 }
 
